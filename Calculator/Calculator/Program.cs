b5922241861ae65a60cf112ccd40bacab9d41f41
Honza_Yang <yangzhaojun666@gmail.com>
2023-09-13 18:48:10 +0200

    hotobá kalkulačka

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -15,32 +15,38 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            /*
-             * Pokud se budes chtit na neco zeptat a zrovna budu pomahat jinde, zkus se zeptat ChatGPT ;) - https://chat.openai.com/
-             * 
-             * ZADANI
-             * Vytvor program ktery bude fungovat jako kalkulacka. Kroky programu budou nasledujici:
-             * 1) Nacte vstup pro prvni cislo od uzivatele (vyuzijte metodu Console.ReadLine() - https://learn.microsoft.com/en-us/dotnet/api/system.console.readline?view=netframework-4.8.
-             * 2) Zkonvertuje vstup od uzivatele ze stringu do integeru - https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/types/how-to-convert-a-string-to-a-number.
-             * 3) Nacte vstup pro druhe cislo od uzivatele a zkonvertuje ho do integeru. (zopakovani kroku 1 a 2 pro druhe cislo)
-             * 4) Nacte vstup pro ciselnou operaci. Rozmysli si, jak operace nazves. Muze to byt "soucet", "rozdil" atd. nebo napr "+", "-", nebo jakkoliv jinak.
-             * 5) Nadefinuj integerovou promennou result a prirad ji prozatimne hodnotu 0.
-             * 6) Vytvor podminky (if statement), podle kterych urcis, co se bude s cisly dit podle zadane operace
-             *    a proved danou operaci - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/selection-statements.
-             * 7) Vypis promennou result do konzole
-             * 
-             * Mozna rozsireni programu pro rychliky / na doma (na poradi nezalezi):
-             * 1) Vypis do konzole pred nactenim kazdeho uzivatelova vstupu co po nem chces
-             * 2) a) Kontroluj, ze uzivatel do vstupu zadal, co mel (cisla, popr. ciselnou operaci). Pokud zadal neco jineho, napis mu, co ma priste zadat a program ukoncete.
-             * 2) b) To same, co a) ale misto ukonceni programu opakovane cti vstup, dokud uzivatel nezada to, co ma
-             *       - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/iteration-statements#the-while-statement
-             * 3) Umozni uzivateli zadavat i desetinna cisla, tedy prekopej kalkulacku tak, aby umela pracovat s floaty
-             */
+            string Input1 = "";
+            string Input2= "";
+            string operation = "";
+            while (!double.TryParse(Input1,out double result)) // způsob, jak zkontrolovat zda je input double jsem si musel dohledat pomocí ChatGPT
+            {
+                Console.WriteLine("zadej platné číslo x");
+                Input1 = Console.ReadLine();
+            }
+            while (!double.TryParse(Input2,out double result))
+            {
+                Console.WriteLine("zadej platné číslo y");
+                Input2 = Console.ReadLine();
+            }
+            while (operation != "+" && operation!= "-")
+            {   Console.WriteLine("zadej operaci +/-");
+                operation = Console.ReadLine(); 
+            }
+            double x = Convert.ToDouble(Input1);
+            double y = Convert.ToDouble(Input2);
+            double Result = 0;
+            if (operation == "-")
+            {
+                Result = x - y;
+                Console.WriteLine(x + operation + y + "=" + Result);
+            }
+            else
+            {
+                Result = x + y;
+                Console.WriteLine(x + operation + y + "=" + Result);
+            }
 
-            //Tento komentar smaz a misto nej zacni psat svuj prdacky kod.
-            string Input1 = Console.ReadLine();
-            Console.WriteLine(Input1);
-            Console.ReadKey(); //Toto nech jako posledni radek, aby se program neukoncil ihned, ale cekal na stisk klavesy od uzivatele.
+                Console.ReadKey(); //Toto nech jako posledni radek, aby se program neukoncil ihned, ale cekal na stisk klavesy od uzivatele.
         }
     }
 }
