d0904e1919368c7ed7d74680fc8e58127fa842e6
Honza Yang <yangzhaojun666@gmail.com>
2023-09-20 11:05:53 +0200

    update

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -15,38 +15,58 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            string Input1 = "";
-            string Input2= "";
-            string operation = "";
-            while (!double.TryParse(Input1,out double result)) // způsob, jak zkontrolovat zda je input double jsem si musel dohledat pomocí ChatGPT
+            string konec = "";
+            while (konec != "ne")
             {
-                Console.WriteLine("zadej platné číslo x");
-                Input1 = Console.ReadLine();
-            }
-            while (!double.TryParse(Input2,out double result))
-            {
-                Console.WriteLine("zadej platné číslo y");
-                Input2 = Console.ReadLine();
-            }
-            while (operation != "+" && operation!= "-")
-            {   Console.WriteLine("zadej operaci +/-");
-                operation = Console.ReadLine(); 
-            }
-            double x = Convert.ToDouble(Input1);
-            double y = Convert.ToDouble(Input2);
-            double Result = 0;
-            if (operation == "-")
-            {
-                Result = x - y;
-                Console.WriteLine(x + operation + y + "=" + Result);
-            }
-            else
-            {
-                Result = x + y;
-                Console.WriteLine(x + operation + y + "=" + Result);
-            }
+                konec = "";
+                string Input1 = "";
+                string Input2= "";
+                string operation = "";
+                while (!double.TryParse(Input1,out double result)) // kontrola jestli je to číslo
+                {
+                    Console.WriteLine("zadej platné číslo x");
+                    Input1 = Console.ReadLine();
+                }
+                while (!double.TryParse(Input2,out double result))
+                {
+                    Console.WriteLine("zadej platné číslo y");
+                    Input2 = Console.ReadLine();
+                }
+                while (operation != "+" && operation!= "-" && operation != "*" && operation != "/") // kontrola jestli je to platná operace
+                {   Console.WriteLine("zadej operaci +;-;*;/");
+                    operation = Console.ReadLine(); 
+                }
+                double x = Convert.ToDouble(Input1);
+                double y = Convert.ToDouble(Input2);
+                double Result = 0;
+                switch (operation)
+                {
+                    case "+":
+                        Result = x + y;
+                        break;
 
-                Console.ReadKey(); //Toto nech jako posledni radek, aby se program neukoncil ihned, ale cekal na stisk klavesy od uzivatele.
+                    case "-":
+                        Result = x - y;
+                        break;
+
+                    case "*":
+                        Result = x * y;
+                        break;
+
+                    case "/":
+                        Result = x / y;
+                    break;
+                }
+                Console.WriteLine(x + operation + y + "=" + Result);
+                while (konec!= "ano"&& konec!= "ne")
+                {
+                    Console.WriteLine("Chceš pokračovat? Zadej ano/ne");
+                    konec= Console.ReadLine();
+                }
+                 
+               
+            }
+            Console.ReadKey(); //Toto nech jako posledni radek, aby se program neukoncil ihned, ale cekal na stisk klavesy od uzivatele.
         }
     }
 }
