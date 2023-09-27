21a37f6415ebc7159f1264c5c05ee90a2cc94c4d
Honza_Yang <yangzhaojun666@gmail.com>
2023-09-27 09:45:23 +0200

    začátek trochu lepší kalkulačky

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO.Pipes;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,8 +17,8 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("zadej typ operace: commonOperation; equation");
-            string whichFunction = "equation";
+            Console.WriteLine("zadej typ operace: commonOperation, equation, advancedCalculator");
+            string whichFunction = "advancedCalculator";
             switch (whichFunction)
             {
                 case "commonOperation":
@@ -25,6 +26,9 @@ namespace Calculator
                     break;
                 case "equation":
                     equationFunction();
+                    break;
+                case "advancedCalculator":
+                    advancedCalculator();
                     break;
             }
             void commonOperation()
@@ -34,119 +38,164 @@ namespace Calculator
                 while (konec != "ne")
                 {
                     konec = "";
-                    string Input1 = "";
-                    string Input2 = "";
+                    string input1 = "";
+                    string input2 = "";
                     string operation = "";
                     double x = 0;
                     double y = 0;
-                    double Result = 0;
-                    while (!double.TryParse(Input1, out double result) && Input1 != "ans") // kontrola jestli je to číslo
+                    double result = 0;
+                    while (!double.TryParse(input1, out double result1) && input1 != "ans") // kontrola jestli je to číslo
                     {
                         Console.WriteLine("zadej platné číslo x");
-                        Input1 = Console.ReadLine();
+                        input1 = Console.ReadLine();
                     }
-                    while (!double.TryParse(Input2, out double result) && Input2 != "ans") // kontrola jestli je to číslo
+                    while (!double.TryParse(input2, out double result1) && input2 != "ans") // kontrola jestli je to číslo
                     {
                         Console.WriteLine("zadej platné číslo y");
-                        Input2 = Console.ReadLine();
+                        input2 = Console.ReadLine();
                     }
                     while (operation != "+" && operation != "-" && operation != "*" && operation != "/") // kontrola jestli je to platná operace
                     {
                         Console.WriteLine("zadej operaci +;-;*;/");
                         operation = Console.ReadLine();
                     }
-                    if (Input1 == "ans")
+                    if (input1 == "ans") // použití výsledku z minulého počítání, nebo nová hodnota
                     {
                         x = ans;
                     }
                     else
                     {
-                        x = Convert.ToDouble(Input1);
+                        x = Convert.ToDouble(input1);
                     }
-                    if (Input2 == "ans")
+                    if (input2 == "ans")
                     {
                         x = ans;
                     }
                     else
                     {
-                        y = Convert.ToDouble(Input2);
+                        y = Convert.ToDouble(input2);
                     }
 
                     switch (operation)
                     {
                         case "+":
-                            Result = x + y;
+                            result = x + y;
                             break;
 
                         case "-":
-                            Result = x - y;
+                            result = x - y;
                             break;
 
                         case "*":
-                            Result = x * y;
+                            result = x * y;
                             break;
 
                         case "/":
-                            Result = x / y;
+                            result = x / y;
                             break;
                     }
-                    Console.WriteLine(x + operation + y + "=" + Result);
-                    ans = Result;
-
-                    while (konec != "ano" && konec != "ne")
+                    Console.WriteLine(x + operation + y + "=" + result);
+                    ans = result;
+                    while (konec != "ano" && konec != "ne") 
                     {
-                        Console.WriteLine("Chceš pokračovat? Zadej ano/ne");
+                        Console.WriteLine("Chceš pokračovat? Zadej ano/ne"); 
                         konec = Console.ReadLine();
                     }
 
                 }
 
 
-            }
+            } // základní zadání kalklačky s ans
             void equationFunction()
-            
-            {
-                Console.WriteLine("Zadej jednoduchou rovnici");
-                string equation = Console.ReadLine()+"_";
-                int orderOfEqual = equation.IndexOf('=');
-                int lengthOfEquation = equation.Length;
-                double variable = 0;
-                string x = "0";
-                for (int i = 0; i < equation.Length; i++)
+            // zatím funguje jenom sčítání a odčítání, je tomu fuk jestli tam neznámou vůbec má lol
+            {while (true)
                 {
-                    if (i <= orderOfEqual)
+                    Console.WriteLine("Zadej jednoduchou rovnici");
+                    string equation = Console.ReadLine() + "_"; // přídává se to aby to ukončilo if na pravo od =
+                    int orderOfEqual = equation.IndexOf('=');
+                    int lengthOfEquation = equation.Length;
+                    double variable = 0;
+                    string x = "0";
+                    int signum = 0;
+                    for (int i = 0; i < equation.Length; i++)
                     {
-                        if (Int32.TryParse(equation[i].ToString(), out int result))
+                        switch (equation[i])
                         {
-                            x += equation[i];
+                            case '+':
+                                signum = 1;
+                                break;
+                            case '-':
+                                signum = -1;
+                                break;
                         }
-                        else
-                        {
-                            variable = variable - Int32.Parse(x);
-                            x = "0";
-                        }
-                    }
-                    if (i > orderOfEqual)
-                    {
-                        if (Int32.TryParse(equation[i].ToString(), out int result))
-                        {
-                            x += equation[i];
-                        }
-                        else
-                        {
-                            variable = variable + Int32.Parse(x);
-                            x = "0";
-                        }
-                        
-                    }
-                    if (i==orderOfEqual)
-                    {
-                        variable = variable - Int32.Parse(x);
-                        x = "0";
-                    }
-                }
-                Console.WriteLine("x = "+ variable);
 
+                        if (i <= orderOfEqual) // pokud je to na stejné straně jako neznámá tak se musí operace otoči
+                        {
+                            if (Int32.TryParse(equation[i].ToString(), out int result)) 
+                            {
+                                x += equation[i]; // kvůli více čiferným číslům
+                            }
+                            else
+                            {
+                                variable = variable - (Int32.Parse(x) * signum); // pokud to narazí na znaménko tak započítá to číslo předtím do výsledku
+                                x = "0";
+                            }
+                        }
+                        if (i > orderOfEqual)
+                        {
+                            if (Int32.TryParse(equation[i].ToString(), out int result))
+                            {
+                                x += equation[i];
+                            }
+                            else
+                            {
+                                variable = variable + (Int32.Parse(x) * signum);
+                                x = "0";
+                            }
+
+                        }
+                        if (i == orderOfEqual) // pokud je poslední číslo napravo záporný tak se to neotočí
+                        {
+                            signum = 1;
+                        }
+                    }
+                    Console.WriteLine("x = " + variable);
+                }
+                
+
+            } // rovnice, zatím moc nefunguje lol
+            void advancedCalculator() // počítání s proměnnou, není limitovaná na 2 čísla (work in progress lol)
+                {
+                double result = 0;
+                int signum = 1;
+                while (true)
+                {
+                    Console.WriteLine("aaaaaa");
+                    string input = Console.ReadLine();
+                    if (!double.TryParse(input, out double result1))
+                    {
+                        switch (input)
+                        {
+                            case "-":
+                                signum = -1;
+                                break;
+                            case "=":
+                                Console.WriteLine("výsledek je " + result);
+                                break;
+                            case "+":
+                                signum = 1;
+                                break;
+
+
+                        }
+                    }
+                    else
+                    {
+                        result += double.Parse(input) * signum;
+                    }
+
+
+                }
             }
             Console.ReadKey(); //Toto nech jako posledni radek, aby se program neukoncil ihned, ale cekal na stisk klavesy od uzivatele.
         } 
