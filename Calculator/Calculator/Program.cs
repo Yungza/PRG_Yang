87f0dacb73eaa3e6b4a2eab0eb9dd0839420bb42
Honza_Yang <yangzhaojun666@gmail.com>
2023-10-12 16:53:14 +0200

    calculator homework definitive version

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -19,72 +19,81 @@ namespace Calculator
         static void Main(string[] args)
         {
             string variable = "0";
-            Console.WriteLine("zadej typ operace: commonOperation, photomath");
-            string whichFunction = "photomath";
-            switch (whichFunction)
+            double ans = 0;
+            while (true)
             {
-                case "commonOperation":
-                    commonOperation();
-                    break;
-                case "equation":
-                    equationFunction();
-                    break;
-                case "photomath":
-                    photomath();
-                    break;
+                string whichFunction = "";
+                while (!IfInt(whichFunction))
+                {
+                    Console.WriteLine("zadej typ operace: 1. Kalkulačka, 2. Photomath(pouze sčítání, odčítání), 3. Různé věci, na vytáhnutí známky kdybych měl HODNĚ špatný coding style");
+                    whichFunction = Console.ReadLine();
+                }
+                switch (whichFunction)
+                {
+                    case "1":
+                        Calculator();
+                        break;
+                    case "equation": // work in progress
+                        EquationFunction();
+                        break;
+                    case "2":
+                        Photomath();
+                        break;
+                    case "3":
+                        string whichFunction2 = "";
+                        while (!IfInt(whichFunction2))
+                        {
+                            Console.WriteLine("na výběr: 1. binární kalkulačka, 2. faktoriál, 3. fibonacciho posloupnost");
+                            whichFunction2 = Console.ReadLine();
+
+                        }
+                        switch (whichFunction2)
+                        {
+                            case "1":
+                                string binary = BinaryCalculator();
+                                Console.WriteLine(binary);
+                                break;
+                            case "2":
+                                string inputFactorial = "";
+                                while (!IfInt(inputFactorial))
+                                {
+                                    Console.WriteLine("zadej přirozené číslo, jehož faktorial chceš vědět");
+                                    inputFactorial = Console.ReadLine();
+                                }
+                                int factorial = FactorialCalculator(int.Parse(inputFactorial));
+                                Console.WriteLine("faktorial čísla " + inputFactorial + " je " + factorial);
+                                break;
+                            case "3":
+                                string inputFibonacci = "";
+                                while (!IfInt(inputFibonacci))
+                                {
+                                    Console.WriteLine("zadej přirozené číslo jehož prvek fibonacciho posloupnosti chceš vědět");
+                                    inputFibonacci = Console.ReadLine();
+                                }
+                                int fibonacci = FibonacciCalculator(int.Parse(inputFibonacci));
+                                Console.WriteLine("pro číslo " + inputFibonacci + " je prvek fibonacciho posloupnosti " + fibonacci);
+                                break;
+                        }
+                        break;
+                }
             }
-            void commonOperation()  // základní zadání kalkulačky s ans, proměnnou x 
+            
+            void Calculator()  // základní zadání kalkulačky s ans, proměnnou x 
             {
                 Console.WriteLine("pro použití minulého výsledku zadej ans při zadávání čísla");
-                double ans = 0;
                 while (true)
                 {
-                    string input1 = "";
-                    string input2 = "";
                     string operation = "";
-                    double x = 0;
-                    double y = 0;
+                    double x = InputToNumber();
+                    Console.WriteLine("první číslo: " + x);
+                    double y = InputToNumber();
+                    Console.WriteLine("druhé číslo: " + y);
                     double result = 0;
-                    while (!double.TryParse(input1, out double result1) && input1 != "ans" && input1 != "x") // kontrola jestli je to platný input
-                    {
-                        Console.WriteLine("zadej platné číslo x");
-                        input1 = Console.ReadLine();
-                        if (input1=="x")
-                        {
-                            input1 = calculatorVariable();
-                        }
-                    }
-                    while (!double.TryParse(input2, out double result1) && input2 != "ans"&& input2 != "x") // kontrola jestli je to číslo/ans
-                    {
-                        Console.WriteLine("zadej platné číslo y");
-                        input2 = Console.ReadLine();
-                        if (input2 == "x")
-                        {
-                            input2 = calculatorVariable();
-                        }
-                    }
                     while (operation != "+" && operation != "-" && operation != "*" && operation != "/") // kontrola jestli je to platná operace
                     {
                         Console.WriteLine("zadej operaci +;-;*;/");
                         operation = Console.ReadLine();
                     }
-                    if (input1 == "ans") // použití výsledku z minulého počítání, nebo nová hodnota
-                    {
-                        x = ans;
-                    }
-                    else
-                    {
-                        x = Convert.ToDouble(input1);
-                    }
-                    if (input2 == "ans")
-                    {
-                        y = ans;
-                    }
-                    else
-                    {
-                        y = Convert.ToDouble(input2);
-                    }
-
                     switch (operation)
                     {
                         case "+":
@@ -106,33 +115,47 @@ namespace Calculator
                     Console.WriteLine(x + operation + y + "=" + result);
                     ans = result;
                     Console.WriteLine("pro ukončení programu napiš libovolný text, v opačném případě zmáčkni pouze enter");
-                    if (Console.ReadLine()!= "")
+                    if (Console.ReadLine()!= "") 
                     {
-                        break;
+                        break; // jakýkoliv input vrátí uživatle na začátek
                     }
                     
                 }
 
-                string calculatorVariable() // počítání s proměnnou
-                {
-                    string input = "";
-                    if (variable == "0")
-                    {
-                        while (!double.TryParse(input, out double result1))
-                        {
-                            Console.WriteLine("Zadej hodnotu neznámé");
-                            input = Console.ReadLine();
-                        }
-                        variable = input;
-                    }
-                    else
-                    {
-                        Console.WriteLine();
-                    }
-                    return variable;
-                }
             }
-            void photomath() // lze zadat celý příklad
+            string CalculatorVariable() 
+            {
+                string input = "";
+                if (variable == "0")
+                {
+                    while (!double.TryParse(input, out double result1))
+                    {
+                        Console.WriteLine("Zadej hodnotu neznámé");
+                        input = Console.ReadLine();
+                    }
+                    variable = input;
+                }
+                return variable;
+            }// neznámá v kalkulačce
+            double InputToNumber()
+            {
+                string input = "";
+                while (!double.TryParse(input, out double result1) && input != "ans" && input != "x") // kontrola platného inputu
+                {
+                    Console.WriteLine("zadej platné číslo, ans, či proměnnou x");
+                    input = Console.ReadLine();
+                }
+                if (input == "x") // zavedení proměnné
+                {
+                    input = CalculatorVariable();
+                }
+                else if (input =="ans") // použití minulého výsledku
+                {
+                    input = ans.ToString();
+                }
+                return double.Parse(input);
+            }
+            void Photomath()
             { while (true)
                 {
                     Console.WriteLine("zadej příklad");
@@ -142,11 +165,11 @@ namespace Calculator
                     int result = 0;
                     if (input.IndexOf("x") != -1)
                     {
-                        equationFunction(); // ještě nefungujeeeeeeeeeeee
+                        EquationFunction(); // ještě nefungujeeeeeeeeeeee
                     }
                     else if (input.IndexOf("*") != -1)
                     {
-                        input = photomathEdit(input);
+                        input = PhotomathEdit(input);
                     }
                     else
                     {
@@ -182,8 +205,8 @@ namespace Calculator
                     }
                 }
                 
-            }
-            void equationFunction()
+            }//nedodělaný, zatím umí jenom sčítat, odčítat
+            void EquationFunction()
             // zatím funguje jenom sčítání a odčítání, je tomu fuk jestli tam neznámou vůbec má lol
             {
                 while (true)
@@ -242,7 +265,7 @@ namespace Calculator
 
 
             } // rovnice, zatím moc nefunguje lol
-            string photomathEdit(string input)
+            string PhotomathEdit(string input)
             {
                 List<char> listEdit = new List<char>(input);
                 Console.WriteLine(listEdit[2]);
@@ -250,9 +273,68 @@ namespace Calculator
                 input = new string(listEdit.ToArray());
                 Console.WriteLine(input);
                 return input;
-            }
+            }// work in progress zzzz
+            string BinaryCalculator()
+            {
+                string remainder = "";
+                string input = "";
+                while (!IfInt(input))
+                {
+                    Console.WriteLine("zadej decimární číslo, které chceš převést do binární soustavy");
+                    input = Console.ReadLine();
+                }
+                int binary = int.Parse(input);
+                while (binary > 0) // způsob jsem našel v komentáři https://www.khanacademy.org/math/algebra-home/alg-intro-to-algebra/algebra-alternate-number-bases/v/large-number-decimal-to-binary
+                {
+                    if (binary % 2 == 0)
+                    {
+                        remainder += "0";
+                    }
+                    else
+                    {
+                        remainder += "1";
+                    }
+                    binary = binary / 2;
+                }
+                remainder.Reverse();
+                return remainder;
 
-            Console.ReadKey(); //Toto nech jako posledni radek, aby se program neukoncil ihned, ale cekal na stisk klavesy od uzivatele.
+            }
+            int FactorialCalculator(int n)
+            {
+                if (n ==1)
+                {
+                    return 1;
+                }
+                int nFactorial = n * FactorialCalculator(n-1);
+                return nFactorial;
+
+            }
+            int FibonacciCalculator(int n)
+            {
+                if (n==0)
+                {
+                    return 0;
+                }
+                if (n==1)
+                {
+                    return 1;
+                }
+                int nFibonacci = FibonacciCalculator(n-1) + FibonacciCalculator(n-2);
+                return nFibonacci;
+            }
+            bool IfInt(string input)
+            {
+                if (int.TryParse(input, out int result))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }// kontrola jestli int
+
         } 
 
     }
