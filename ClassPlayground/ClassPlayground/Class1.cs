66e0da38e04ef0599f6e57765b94d11233d77a10
Honza Yang <yangzhaojun666@gmail.com>
2023-12-06 11:24:27 +0100

    hodina 6.12

---
+++ ClassPlayground/ClassPlayground/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPlayground
+{
+    internal class Rectangle
+    {
+        public float width, height;
+        public Rectangle(float width, float height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public Rectangle(float width)
+        {
+            this.width = height = width;
+        }
+        public float CalculateArea()
+        {
+            float area = width * height;
+            return area;
+        }
+        public string CalculateAspectRatio()
+        {
+            string shape;
+            if (width == height)
+            {
+                shape = "čtverec";
+                return shape;
+            }
+            if (height > width)
+            {
+                shape = "vysoký";
+            }
+            else
+            {
+                shape = "široký";
+            }
+
+            return shape;
+        }
+        public string ContainsPoint(int x, int y)
+        {
+            if (x <= width)
+            {
+                if (y <= height)
+                {
+                    return $"ano, bod [{x} ; {y}] se nachází v obdélníku / čtverci";
+                }
+                else
+                {
+                    return $"ne, bod [{x} ; {y}] se nenachází v obdélníku / čtverci";
+                }
+            }
+            else
+            {
+                return $"ne, bod [{x} ; {y}] se nenachází v obdélníku / čtverci";
+            }
+        }
+    }
+}
---
+++ ClassPlayground/ClassPlayground/Class2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPlayground
+{
+    /*2) Vytvoř třídu BankAccount, kterou budeme reprezentovat bankovní účet
+ * Přidej třídě BankAccount čtyři proměnné - accountNumber jako číslo účtu
+ *                                            - holderName jako jméno osoby, které účet patří
+ *                                            - currency jako měna, ve které je účet vedený
+ *                                            - balance jako zůstatek na účtu
+ * Přidej třídě BankAccount čtyři funkce - Deposit, která jako vstupní parametr přijme množství peněz a vloží je na účet
+ *                                          - Withdraw, která jako vstupní parametr přijme množství peněz a z účtu "vybere" peníze, tedy sníží zůstatek a navrátí požadované množství
+ * Pokud na účtu není dostatek peněz, uživatele upozorní a vrátí nulu.
+ *                                          - Transfer, která jako vstupní parametry přijme množství peněz a číslo účtu, na které se budou peníze posílat, a převede peníze
+ * z jednoho účtu na druhý(opět pouze pokud je na účtu, ze kterého převod jde, dostatek peněz)
+ * Přidej třídě BankAccount konstruktor, který bude přijímat dva parametry - jméno majitele účtu a měnu, ve které bude účet vedený
+ *                                                                            - Při jeho zavolání nastav jméno a měnu podle vstupních parametrů, accountNumber nastav jako náhodně
+ * vygenerované číslo velké alespoň 100 000 000 a menší, než 10 000 000 000 a balance nastav na nulu
+ */
+    internal class BankAccount
+    {
+        int accountNumber; string holderName; string currency; int balance;
+        public BankAccount(int accountNumber, string holderName, string currency, int balance)
+        {
+            Dictionary<int, BankAccount> LinkNum = new Dictionary<int, BankAccount>();
+            LinkNum[accountNumber] = BankAccount;
+            this.accountNumber = accountNumber;
+            this.holderName = holderName;
+            this.currency = currency;
+            this.balance = balance;
+
+        }
+        public int Deposit(int amount)
+        {
+            balance = balance + amount;
+            return balance;
+        }
+        public int Witdhdraw(int amount)
+        {
+            if (balance < amount)
+            {
+                Console.WriteLine($"tolik peněz nemáš wole, vybral jsi {balance} peněz");
+                return 0;
+            }
+            balance = balance - amount;
+            return balance;
+        }
+        public int Transfer(int amount)
+        {
+            return balance;
+        }
+    }
+}
