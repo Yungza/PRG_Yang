8484db187099a0f399259ba27743e1a8cc8ed06e
Honza Yang <yangzhaojun666@gmail.com>
2024-03-20 11:19:59 +0100

    HW malování

---
+++ HW_malovani/HW_malovani/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace HW_malovani
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_red = new System.Windows.Forms.Button();
+            this.button_green = new System.Windows.Forms.Button();
+            this.button_blue = new System.Windows.Forms.Button();
+            this.button_white = new System.Windows.Forms.Button();
+            this.button_black = new System.Windows.Forms.Button();
+            this.textBox_size = new System.Windows.Forms.TextBox();
+            this.button_up = new System.Windows.Forms.Button();
+            this.button_down = new System.Windows.Forms.Button();
+            this.button_run = new System.Windows.Forms.Button();
+            this.button_pink = new System.Windows.Forms.Button();
+            this.button_orange = new System.Windows.Forms.Button();
+            this.button_purple = new System.Windows.Forms.Button();
+            this.button_yellow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_red
+            // 
+            this.button_red.BackColor = System.Drawing.Color.Red;
+            this.button_red.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button_red.Location = new System.Drawing.Point(9, 7);
+            this.button_red.Name = "button_red";
+            this.button_red.Size = new System.Drawing.Size(26, 26);
+            this.button_red.TabIndex = 0;
+            this.button_red.UseVisualStyleBackColor = false;
+            this.button_red.Click += new System.EventHandler(this.button_red_Click);
+            // 
+            // button_green
+            // 
+            this.button_green.BackColor = System.Drawing.Color.Green;
+            this.button_green.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button_green.Location = new System.Drawing.Point(73, 7);
+            this.button_green.Name = "button_green";
+            this.button_green.Size = new System.Drawing.Size(26, 26);
+            this.button_green.TabIndex = 1;
+            this.button_green.UseVisualStyleBackColor = false;
+            this.button_green.Click += new System.EventHandler(this.button_green_Click);
+            // 
+            // button_blue
+            // 
+            this.button_blue.BackColor = System.Drawing.Color.Blue;
+            this.button_blue.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button_blue.Location = new System.Drawing.Point(41, 7);
+            this.button_blue.Name = "button_blue";
+            this.button_blue.Size = new System.Drawing.Size(26, 26);
+            this.button_blue.TabIndex = 2;
+            this.button_blue.UseVisualStyleBackColor = false;
+            this.button_blue.Click += new System.EventHandler(this.button_blue_Click);
+            // 
+            // button_white
+            // 
+            this.button_white.BackColor = System.Drawing.Color.White;
+            this.button_white.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button_white.Location = new System.Drawing.Point(137, 7);
+            this.button_white.Name = "button_white";
+            this.button_white.Size = new System.Drawing.Size(26, 26);
+            this.button_white.TabIndex = 4;
+            this.button_white.UseVisualStyleBackColor = false;
+            this.button_white.Click += new System.EventHandler(this.button_white_Click);
+            // 
+            // button_black
+            // 
+            this.button_black.BackColor = System.Drawing.Color.Black;
+            this.button_black.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button_black.Location = new System.Drawing.Point(105, 7);
+            this.button_black.Name = "button_black";
+            this.button_black.Size = new System.Drawing.Size(26, 26);
+            this.button_black.TabIndex = 5;
+            this.button_black.UseVisualStyleBackColor = false;
+            this.button_black.Click += new System.EventHandler(this.button_black_Click);
+            // 
+            // textBox_size
+            // 
+            this.textBox_size.Location = new System.Drawing.Point(1022, 22);
+            this.textBox_size.Name = "textBox_size";
+            this.textBox_size.Size = new System.Drawing.Size(27, 20);
+            this.textBox_size.TabIndex = 6;
+            this.textBox_size.Text = "2";
+            this.textBox_size.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button_up
+            // 
+            this.button_up.Location = new System.Drawing.Point(1055, 7);
+            this.button_up.Name = "button_up";
+            this.button_up.Size = new System.Drawing.Size(27, 19);
+            this.button_up.TabIndex = 7;
+            this.button_up.Text = "▲";
+            this.button_up.UseVisualStyleBackColor = true;
+            this.button_up.Click += new System.EventHandler(this.button_up_Click);
+            // 
+            // button_down
+            // 
+            this.button_down.Location = new System.Drawing.Point(1055, 32);
+            this.button_down.Name = "button_down";
+            this.button_down.Size = new System.Drawing.Size(27, 19);
+            this.button_down.TabIndex = 8;
+            this.button_down.Text = "▼";
+            this.button_down.UseVisualStyleBackColor = true;
+            this.button_down.Click += new System.EventHandler(this.button_down_Click);
+            // 
+            // button_run
+            // 
+            this.button_run.BackColor = System.Drawing.Color.Aqua;
+            this.button_run.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button_run.Location = new System.Drawing.Point(9, 39);
+            this.button_run.Name = "button_run";
+            this.button_run.Size = new System.Drawing.Size(26, 26);
+            this.button_run.TabIndex = 9;
+            this.button_run.Text = ":)";
+            this.button_run.UseVisualStyleBackColor = false;
+            // 
+            // button_pink
+            // 
+            this.button_pink.BackColor = System.Drawing.Color.HotPink;
+            this.button_pink.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button_pink.Location = new System.Drawing.Point(137, 39);
+            this.button_pink.Name = "button_pink";
+            this.button_pink.Size = new System.Drawing.Size(26, 26);
+            this.button_pink.TabIndex = 10;
+            this.button_pink.UseVisualStyleBackColor = false;
+            this.button_pink.Click += new System.EventHandler(this.button_pink_Click);
+            // 
+            // button_orange
+            // 
+            this.button_orange.BackColor = System.Drawing.Color.Orange;
+            this.button_orange.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button_orange.Location = new System.Drawing.Point(105, 39);
+            this.button_orange.Name = "button_orange";
+            this.button_orange.Size = new System.Drawing.Size(26, 26);
+            this.button_orange.TabIndex = 11;
+            this.button_orange.UseVisualStyleBackColor = false;
+            this.button_orange.Click += new System.EventHandler(this.button_orange_Click);
+            // 
+            // button_purple
+            // 
+            this.button_purple.BackColor = System.Drawing.Color.Purple;
+            this.button_purple.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button_purple.Location = new System.Drawing.Point(73, 39);
+            this.button_purple.Name = "button_purple";
+            this.button_purple.Size = new System.Drawing.Size(26, 26);
+            this.button_purple.TabIndex = 12;
+            this.button_purple.UseVisualStyleBackColor = false;
+            this.button_purple.Click += new System.EventHandler(this.button_purple_Click);
+            // 
+            // button_yellow
+            // 
+            this.button_yellow.BackColor = System.Drawing.Color.Yellow;
+            this.button_yellow.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button_yellow.Location = new System.Drawing.Point(41, 39);
+            this.button_yellow.Name = "button_yellow";
+            this.button_yellow.Size = new System.Drawing.Size(26, 26);
+            this.button_yellow.TabIndex = 13;
+            this.button_yellow.UseVisualStyleBackColor = false;
+            this.button_yellow.Click += new System.EventHandler(this.button_yellow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1094, 603);
+            this.Controls.Add(this.button_yellow);
+            this.Controls.Add(this.button_purple);
+            this.Controls.Add(this.button_orange);
+            this.Controls.Add(this.button_pink);
+            this.Controls.Add(this.button_run);
+            this.Controls.Add(this.button_down);
+            this.Controls.Add(this.button_up);
+            this.Controls.Add(this.textBox_size);
+            this.Controls.Add(this.button_black);
+            this.Controls.Add(this.button_white);
+            this.Controls.Add(this.button_blue);
+            this.Controls.Add(this.button_green);
+            this.Controls.Add(this.button_red);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_red;
+        private System.Windows.Forms.Button button_green;
+        private System.Windows.Forms.Button button_blue;
+        private System.Windows.Forms.Button button_white;
+        private System.Windows.Forms.Button button_black;
+        private System.Windows.Forms.TextBox textBox_size;
+        private System.Windows.Forms.Button button_up;
+        private System.Windows.Forms.Button button_down;
+        private System.Windows.Forms.Button button_run;
+        private System.Windows.Forms.Button button_pink;
+        private System.Windows.Forms.Button button_orange;
+        private System.Windows.Forms.Button button_purple;
+        private System.Windows.Forms.Button button_yellow;
+    }
+}
+
---
+++ HW_malovani/HW_malovani/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW_malovani
+{
+    public partial class Form1 : Form
+    {
+        public Brush brush = Brushes.Black;
+        public bool mouse = false;
+        Point previousLocation = new Point();
+        int size = 1;
+        public Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_red_Click(object sender, EventArgs e)
+        {
+            brush = Brushes.Red;
+        }
+
+        private void button_blue_Click(object sender, EventArgs e)
+        {
+            brush = Brushes.Blue;
+        }
+
+        private void button_green_Click(object sender, EventArgs e)
+        {
+            brush = Brushes.Green;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouse = false;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouse = true;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Point currentPosition = e.Location;
+            int x = rnd.Next(0,1110);
+            int y = rnd.Next(0,642);
+            Point randomPosition = new Point(x,y);
+            Pen pen = new Pen(brush, size);
+            if (mouse)
+            {
+                g.DrawLine(pen, currentPosition, previousLocation);
+            }
+            previousLocation = currentPosition;
+            if (currentPosition.X > button_run.Location.X -20 && currentPosition.X < button_run.Location.X + 20 + button_run.Width)
+            {
+                if (currentPosition.Y > button_run.Location.Y -20 && currentPosition.Y < button_run.Location.Y + 20 + button_run.Height)
+                {
+                    button_run.Location = randomPosition;
+                }
+            }
+        }
+
+        private void button_black_Click(object sender, EventArgs e)
+        {
+            brush = Brushes.Black;
+        }
+
+        private void button_white_Click(object sender, EventArgs e)
+        {
+            brush = Brushes.White;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox_size.Text, out int result))
+            {
+                size = Convert.ToInt32(textBox_size.Text);
+            }
+            else
+            {
+                MessageBox.Show("zadej číslo ty hajzle");
+            }
+        }
+
+        private void button_up_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox_size.Text);
+            x++;
+            textBox_size.Text = x.ToString();
+        }
+
+        private void button_down_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox_size.Text);
+            x--;
+            textBox_size.Text = x.ToString();
+        }
+
+        private void button_yellow_Click(object sender, EventArgs e)
+        {
+            brush = Brushes.Yellow;
+        }
+
+        private void button_purple_Click(object sender, EventArgs e)
+        {
+            brush = Brushes.Purple;
+        }
+
+        private void button_orange_Click(object sender, EventArgs e)
+        {
+            brush = Brushes.Orange;
+        }
+
+        private void button_pink_Click(object sender, EventArgs e)
+        {
+            brush = Brushes.Pink;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
